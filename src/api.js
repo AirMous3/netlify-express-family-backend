be77a284b82ae2403c71665a29aacfa402480c83
AirMouse <airmouselol@gmail.com>
2022-12-19 08:21:05 +0300

    init commit

---
+++ src/api.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const serverless = require('serverless-http');
+// const proxy = require('express-http-proxy');
+const path = require('path');
+var httpProxy = require('http-proxy');
+const {getHandler} = require('./cors/cors-anywhere');
+
+const app = express();
+const router = express.Router();
+
+var proxy = httpProxy.createServer({
+    xfwd: true,            // Append X-Forwarded-* headers
+    secure: process.env.NODE_TLS_REJECT_UNAUTHORIZED !== '0',
+});
+
+const requestHandler = getHandler({}, proxy);
+
+app.use('/.netlify/functions/api', router);
+
+router.use('/', requestHandler);
+
+module.exports = app;
+module.exports.handler = serverless(app);
+
